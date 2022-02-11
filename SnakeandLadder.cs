72e021d41e988a1b2ee73a9a88a90154f4f4aa56
sarvesh <sarveshkumar452000@gmail.com>
2022-02-10 19:30:12 -0800

    Random number added Successfully

--- SnakeandLadder.cs
+++ SnakeandLadder.cs
@@ -9,12 +9,25 @@ namespace SnakeandLadderGame
     internal class SnakeandLadder
     {
         private int Position = 0;
+         
+        Random number = new Random();
+       
 
         public void StartsGame()
         {
             Console.WriteLine("Game Starts Now");
             Console.WriteLine("You are position is " + Position);
+            Console.WriteLine("You are position is " + Position);
         }
+
+        public void RandomNumber()
+        {
+            int Number = number.Next(1, 7);
+            Position += Number;
+            StartsGame();
+            Console.ReadLine();
+        }
+
 
 
     }
