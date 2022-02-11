469cf06e3e01a891240b1f0cf903c0fb9f57fc09
sarvesh <sarveshkumar452000@gmail.com>
2022-02-10 19:15:28 -0800

    Welcome to Snake and ladder Game

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Game");
+            SnakeandLadder snake = new SnakeandLadder();
+            snake.StartsGame();
+            Console.ReadLine();
+        }
+    }
+}
