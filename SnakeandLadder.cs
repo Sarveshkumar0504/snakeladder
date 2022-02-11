7e6a2a937e90eb495d2e8fc669ea6a31e28a7aeb
sarvesh <sarveshkumar452000@gmail.com>
2022-02-10 19:56:44 -0800

    Option numbers added successfully

--- SnakeandLadder.cs
+++ SnakeandLadder.cs
@@ -10,12 +10,56 @@ namespace SnakeandLadderGame
     {
         private int Position = 0;
 
+        
+        Random number = new Random();
+        Random number1 = new Random();
+
+
         public void StartsGame()
         {
-            Console.WriteLine("Game Starts Now");
-            Console.WriteLine("You are position is " + Position);
+            Console.WriteLine("Your position is " + Position);
         }
 
 
+       
+        public void RandomNumber()
+        {
+            int Number = number.Next(1, 7);
+            Position += Number;
+            StartsGame();
+            Option(Number);
+            Console.ReadLine();
+        }
+
+        
+        public void Option(int Number)
+        {
+            int OptionForPlay = number.Next(0, 3);
+
+            switch (OptionForPlay)
+            {
+                case 0:
+                    Console.WriteLine("No Play");
+                    Console.WriteLine("Your Position is same");
+                    break;
+
+                case 1:
+                    Console.WriteLine("You Got A Ladder!!");
+                    Position += Number;
+                    StartsGame();
+                    break;
+
+                case 2:
+                    Console.WriteLine("Oh No!! Snake Trap ");
+                    Position -= Number;
+                    StartsGame();
+                    break;
+
+                default:
+                    Console.WriteLine("Error!!");
+                    StartsGame();
+                    break;
+            }
+           }
+        }
     }
-}
